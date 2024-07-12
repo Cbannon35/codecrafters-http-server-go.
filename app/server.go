1f1587df3981228ad9a58a0c72be03978cf28817
Christopher Bannon <bannon.c.35@berkeley.edu>
2024-07-11 22:52:20 -0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,24 +4,66 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+func setupListener() net.Listener {
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	connection, err := l.Accept()
+	return listener
+}
+
+func writeToConnection(conn net.Conn, message []byte) {
+	_, err := conn.Write([]byte(message))
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Can't write to connection: ", err.Error())
+		return
+	}
+}
+
+func parseRequest(conn net.Conn) []string {
+	req := make([]byte, 1024)
+	n, err := conn.Read(req)
+	if err != nil {
+		fmt.Println("Can't read from connection: ", err.Error())
+		return nil
+	}
+	return strings.Split(string(req[:n]), "\r\n")
+}
+
+func main() {
+	l := setupListener()
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Connection accepted")
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Handling connection")
+	request := parseRequest(conn)
+
+	if request == nil || len(request) < 3 {
+		fmt.Println("Invalid request?")
+		return
 	}
 
-	fmt.Println("Connection accepted")
-	// Respond with http 200
-	connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	// Close the connection
-	defer l.Close()
+	requestLine := strings.Split(request[0], " ")
+
+	if requestLine[1] != "/" {
+		writeToConnection(conn, []byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	} else {
+		writeToConnection(conn, []byte("HTTP/1.1 200 OK\r\n\r\n"))
+	}
+	conn.Close()
 }
