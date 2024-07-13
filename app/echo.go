e57cf8d51dedb5875493df9791f7faf4b6488fab
Christopher Bannon <bannon.c.35@berkeley.edu>
2024-07-12 18:33:11 -0700

    codecrafters submit [skip ci]

--- app/echo.go
+++ app/echo.go
@@ -9,9 +9,14 @@ import (
 func handleEchoRoute(conn net.Conn, req *HTTPRequest) {
 	content := strings.TrimPrefix(req.Path, "/echo/")
 	headers := []string{"Content-Type: text/plain", "Content-Length: " + fmt.Sprint(len(content))}
-	if req.Headers["Accept-Encoding"] == "gzip" {
-		// content = gzipContent(content)
-		headers = append(headers, "Content-Encoding: gzip")
+	
+	encodings := req.Headers["Accept-Encoding"]
+	for _, encoding := range strings.Split(encodings, ",") {
+		if strings.Trim(encoding, " ") == "gzip" {
+			// content = gzipContent(content)
+			headers = append(headers, "Content-Encoding: gzip")
+			break
+		}
 	}
 	response := createResponse("HTTP/1.1 200 OK", headers, content)
 	writeToConnection(conn, []byte(response))
